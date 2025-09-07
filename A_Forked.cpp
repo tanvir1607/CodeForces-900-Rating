d50f41c196c36b244b401e293a448d429ef13c9d
tanvir1607 <10viranik@gmail.com>
2025-09-07 11:42:45 +0600

    CPP

--- A_Forked.cpp
+++ A_Forked.cpp
@@ -5,12 +5,12 @@ typedef long long ll;
 #define endl '\n'
 #define optimize() ios::sync_with_stdio(false); cin.tie(nullptr);
 
+int dx[4] = {1, 1, -1, -1}, 
+    dy[4] = {1, -1, 1, -1};
+
 int main() 
 {
     optimize();
-
-    int dx[4] = {1, 1, -1, -1}, 
-        dy[4] = {1, -1, 1, -1};
 
     int test;
     cin >> test;
@@ -19,17 +19,16 @@ int main()
         ll a, b, kingX, kingY, queenX, queenY;
         cin >> a >> b >> kingX >> kingY >> queenX >> queenY;
 
-        set<pair<ll,ll>> king_hits, queen_hits;
+        set<pair<ll, ll>> king_pos, queen_pos;
         for(int i = 0; i < 4; i++)
         {
-            king_hits.insert({kingX + dx[i] * a, kingY + dy[i] * b});
-            king_hits.insert({kingX + dx[i] * b, kingY + dy[i] * a});
-
-            queen_hits.insert({queenX + dx[i] * a, queenY + dy[i] * b});
-            queen_hits.insert({queenX + dx[i] * b, queenY + dy[i] * a});
+            king_pos.insert({kingX + dx[i] * a, kingY + dy[i] * b});
+            king_pos.insert({kingX + dx[i] * b, kingY + dy[i] * a});
+            queen_pos.insert({queenX + dx[i] * a, queenY + dy[i] * b});
+            queen_pos.insert({queenX + dx[i] * b, queenY + dy[i] * a});
         }
         int ans = 0;
-        for (auto position : king_hits) ans += queen_hits.count(position);
+        for (auto position : king_pos) ans += queen_pos.count(position);
         cout << ans << endl;
     }
     return 0;
