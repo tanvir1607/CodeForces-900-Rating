26a408477f10aa54322e98c946169ec44315a0bf
tanvir1607 <10viranik@gmail.com>
2025-10-20 00:59:18 +0600

    CPP

--- B_Longest_Divisors_Interval.cpp
+++ B_Longest_Divisors_Interval.cpp
@@ -1,3 +1,35 @@
+// Approach - 01
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl '\n'
+#define optimize() ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int main() 
+{
+    optimize();
+
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        ll num;
+        cin >> num;
+
+        int ans = 0;
+        for (int i = 1; num % i == 0; i++) ans++;
+        cout << ans << endl;
+    }
+
+    return 0;
+}
+
+
+
+
+
+// Approach - 02
 #include <bits/stdc++.h>
 using namespace std;
 
